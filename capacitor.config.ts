47f2da58a29da070cdee203a39dbeaab98106cf6
Nour Krimesh <nour.krimesh@gmail.com>
2023-09-25 22:46:29 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interview-dm-ng-modules',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
